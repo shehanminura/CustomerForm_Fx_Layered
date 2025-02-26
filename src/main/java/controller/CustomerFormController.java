952b87c2927de38aa2d9fa11975a698559c07370
shehanminura <shehanminuradp@gmail.com>
2025-02-26 10:15:59 +0530

    create controler and start service pacage

---
+++ src/main/java/controller/CustomerFormController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import dto.Customer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CustomerFormController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableView<?> tblCustomers;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtSalary;
+
+    @FXML
+    private JFXTextField txtid;
+
+    @FXML
+    void OnAddAction(ActionEvent event) {
+        String id = txtid.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        Double salary = Double.parseDouble(txtSalary.getText());
+        Customer customer = new Customer(id,name,address,salary);
+    }
+
+
+    @FXML
+    void OnDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnSeartchAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnUpdateAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGetCustomerReportOnAddAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRelodeOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/edu/icet/ecom/Stater.java
@@ -0,0 +1,11 @@
+package edu.icet.ecom;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Stater extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
